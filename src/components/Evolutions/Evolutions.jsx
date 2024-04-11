19c4a923a30ac8305ad0ed17b8e32c634c11af71
Vanessa Rezk <rezkvane@gmail.com>
2024-04-11 19:25:20 +0200

    evolutions

--- src/components/Evolutions/Evolutions.jsx
+++ src/components/Evolutions/Evolutions.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
-import { colorTypeGradients } from "../../utils/utils";
+
 import "./Evolutions.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { Card, CardImg, Badge, Button } from "reactstrap";
 
 function Pokemon(params) {
   const [pokemon, setPokemon] = useState([]);
@@ -17,10 +16,10 @@ function Pokemon(params) {
     const response = params.poke.url;
     axios.get(response).then(async (response) => {
       const respuesta = response.data;
-      if (respuesta.sprites.other.dream_world.front_default != null) {
+      if (respuesta.sprites.other["official-artwork"].front_default != null) {
         setImagen(respuesta.sprites.other["official-artwork"].front_default);
       } else {
-        setImagen(respuesta.sprites.dream_world.front_default);
+        setImagen(respuesta.sprites.other.dream_world.front_default);
       }
       setPokemon(respuesta);
     });
